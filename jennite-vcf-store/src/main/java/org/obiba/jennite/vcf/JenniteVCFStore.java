689c90d7970acc611bf282e400f312e12ccc2e97
Cedric Fontin <cfontin@maelstrom-research.org>
2017-03-23 17:50:54 -0400

    OPAL-2866 - add code for option to exclude sampleIds in file

--- jennite-vcf-store/src/main/java/org/obiba/jennite/vcf/JenniteVCFStore.java
+++ jennite-vcf-store/src/main/java/org/obiba/jennite/vcf/JenniteVCFStore.java
@@ -201,7 +201,17 @@ public class JenniteVCFStore implements VCFStore {
   }
 
   @Override
+  public void readVCF(String vcfName, OutputStream out, Collection<String> samples, boolean includeSamples) throws NoSuchElementException, IOException {
+    readVCF(vcfName, getFormat(vcfName), out, samples, includeSamples);
+  }
+
+  @Override
   public void readVCF(String vcfName, Format format, OutputStream out, Collection<String> samples) throws NoSuchElementException, IOException {
+    readVCF(vcfName, format, out, samples, true);
+  }
+
+  @Override
+  public void readVCF(String vcfName, Format format, OutputStream out, Collection<String> samples, boolean includeSamples) throws NoSuchElementException, IOException {
     if (!hasVCF(vcfName)) throw new NoSuchElementException("No VCF with name '" + vcfName + "' can be found");
     if (samples == null || samples.isEmpty()) {
       readVCF(vcfName, format, out);
@@ -223,7 +233,7 @@ public class JenniteVCFStore implements VCFStore {
 
     String outputType = Format.VCF == format ? "z" : "b";
     int status = runProcess(vcfName, bcftools("view",
-        "--samples-file", samplesFile.getAbsolutePath(),
+        "--samples-file", includeSamples ? samplesFile.getAbsolutePath() : "^" + samplesFile.getAbsolutePath(),
         "--force-samples", // do not fail if there are unknown samples
         "--output-type", outputType, // compressed VCF/BCF
         "--output-file", outputFile.getAbsolutePath(),
